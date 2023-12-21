e703c7ed3fb7013cb5bb01d535bdd2dd60027cbb
Paul <pasha.makey@gmail.com>
2023-12-21 09:56:33 +0300

    Initial Commit

---
+++ MotorPool/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MotorPool
+//
+//  Created by Paul Makey on 21.12.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
