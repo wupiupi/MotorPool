5d946aff27bf487165caa20c8f9d32d4851c5a06
Paul <pasha.makey@gmail.com>
2023-12-24 19:59:55 +0300

    🔧 chore: Added necessary methods, outlets, etc.

--- MotorPool/ViewControllers/DetailsViewController.swift
+++ MotorPool/ViewControllers/DetailsViewController.swift
@@ -8,6 +8,30 @@
 import UIKit
 
 final class DetailsViewController: UIViewController {
+    
+    // MARK: - Variables
     var user: User!
+    
+    
+    // MARK: - IB Outlets
+    @IBOutlet var carModelLabel: UILabel!
+    
+    @IBOutlet var carImageView: UIImageView!
+    
+    // MARK: - LifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        carImageView.layer.cornerRadius = carImageView.layer.frame.width / 2
+        
+    }
+    
+    // MARK: - IB Action
+    @IBAction private func doneButtonDidTapped() {
+        dismiss(animated: true)
+    }
 }
 
