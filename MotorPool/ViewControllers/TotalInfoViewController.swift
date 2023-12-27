18c68e2fd0155212a3fd7c3e9b289d53f28588c7
Pavel Gribachev <PGribachev@yandex.ru>
2023-12-27 03:28:46 +0300

    Changes were made to the view, a car selection button was added, a custom view was created to display the car and description, an animation and car counting function was added

--- MotorPool/ViewControllers/TotalInfoViewController.swift
+++ MotorPool/ViewControllers/TotalInfoViewController.swift
@@ -9,29 +9,123 @@ import UIKit
 
 final class TotalInfoViewController: UIViewController {
     
+    // MARK: Outlets
     @IBOutlet var carCountLabel: UILabel!
     @IBOutlet var carFavoriteLabel: UILabel!
     @IBOutlet var carTotalCostLabel: UILabel!
     @IBOutlet var carTotalFuelLabel: UILabel!
+    @IBOutlet var chooseRandomCarButton: UIButton!
     
+    @IBOutlet var customStackView: UIStackView!
+    @IBOutlet var customImageView: UIImageView!
+    @IBOutlet var customLabel: UILabel!
+    @IBOutlet var customButton: UIButton!
+    @IBOutlet var customView: UIView!
+    
+    @IBOutlet var blurEffect: UIVisualEffectView!
+    
+    // MARK: Properties
     var user: User!
-        
+    var effect: UIVisualEffect!
+    
+    // MARK: ViewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
         updateUI()
     }
     
-    private func updateUI() {
+    // MARK: Action
+    @IBAction func customButtonDidTapped() {
+        customStackView.isHidden = true
+        blurEffect.isHidden = true
+    }
+    
+    @IBAction func chooseRandomCarDidTapped() {
+        var carDict: [String: String] = [:]
         
+        let carModels = user.autos.map { $0.model }
+        let carBrands = user.autos.map { $0.brand }
+        
+        for (model, brand) in zip(carModels, carBrands) {
+            carDict[model] = brand
+        }
+
+        let randomCar = carDict.randomElement()
+        customStackView.isHidden = false
+        customImageView.image = UIImage(named: "\(randomCar?.key ?? "") \(randomCar?.value ?? "")")
+        customLabel.text = "\(randomCar?.key ?? "") \(randomCar?.value ?? "")"
+        
+        blurEffect.layer.backgroundColor = UIColor(patternImage: .back).cgColor
+        animatedEffectFromView()
+    }
+}
+
+private extension TotalInfoViewController {
+    
+    // MARK: Update main UI
+    func updateUI() {
         user.autos.forEach { auto in
             carCountLabel.text = String(user.autos.count)
             carFavoriteLabel.text = favoriteCar()
             carTotalCostLabel.text = totalCost()
             carTotalFuelLabel.text = totalFuel()
         }
+        
+        // change button style "chooseRandomCar"
+        chooseRandomCarButton.layer.cornerRadius = 10
+        chooseRandomCarButton.titleLabel?.font = .boldSystemFont(ofSize: 16)
+        chooseRandomCarButton.layer.borderWidth = 1
+        chooseRandomCarButton.layer.borderColor = UIColor.black.cgColor
+        
+        updateCustomView()
+        
+        effect = blurEffect.effect
+        blurEffect.isHidden = true
     }
     
-    private func totalCost() -> String {
+    // MARK: Update custom UI
+    func updateCustomView() {
+        
+        //custom stackView
+        customStackView.isHidden = true
+        
+        //custom imageView
+        customImageView.layer.cornerRadius = 10
+        
+        // custom view
+        customView.layer.cornerRadius = 10
+        customView.layer.borderWidth = 1
+        customView.layer.borderColor = UIColor.black.cgColor
+        
+        // custom button
+        customButton.layer.cornerRadius = 10
+        customButton.layer.borderWidth = 1
+        customButton.backgroundColor = .black
+        customButton.tintColor = .white
+        customButton.titleLabel?.font = .boldSystemFont(ofSize: 16)
+        customButton.setTitle("GET STARTED 😈", for: .normal)
+        
+        // custom label
+        customLabel.font = .systemFont(ofSize: 25)
+    }
+    
+    func animatedEffectFromView() {
+        self.blurEffect.isHidden = false
+        view.addSubview(customStackView)
+        customStackView.center = view.center
+        
+        customView.transform = CGAffineTransform.init(scaleX: 1.3, y: 1.3)
+        customView.alpha = 0
+        
+        UIView.animate(withDuration: 1.5) {
+            self.blurEffect.effect = self.effect
+            self.customView.alpha = 1
+            self.customView.transform = CGAffineTransform.identity
+        }
+    }
+    
+    // MARK: Calculating the total price of cars
+    func totalCost() -> String {
         var totalCost = 0.0
         user.autos.forEach { totalCost += $0.price }
         
@@ -43,15 +137,16 @@ final class TotalInfoViewController: UIViewController {
         return "\(stringCost) $"
     }
     
-    private func totalFuel() -> String {
+    // MARK: Counting the total amount of fuel
+    func totalFuel() -> String {
         var totalFuel = 0.0
-        
         user.autos.forEach { totalFuel += $0.fuelConsumption }
         
         return String(format: "%.1f l/100 km" , totalFuel)
     }
     
-    private func favoriteCar() -> String {
+    // MARK: Identifying the most frequently encountered vehicle
+    func favoriteCar() -> String {
         var favoriteCars: [String: Int] = [:]
         let modelCars = user.autos.map { $0.model }
         
@@ -64,6 +159,4 @@ final class TotalInfoViewController: UIViewController {
         
         return mostFavoriteCar
     }
-
-    
 }
